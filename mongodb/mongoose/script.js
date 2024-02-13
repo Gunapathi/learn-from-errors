508d3d5155194a71a4599de94022db25c9b06cce
Gunapathi <gunapathi123@gmail.com>
2024-02-13 23:59:50 +0530

    schema separation

--- mongodb/mongoose/script.js
+++ mongodb/mongoose/script.js
@@ -5,17 +5,26 @@ mongoose.connect('mongodb://localhost:27017/mongooseBasic')
     .then(() => console.log('DB connection established'))
     .catch((err) => console.error('Error connecting to Mongo'));
 
-const user = new User({
-    name: "Guna",
-    age: 28
-});
+// const user = new User({
+//     name: "Guna",
+//     age: 28
+// });
 
 // user.save().then((res, req) => {
 //     console.log('user saved successfully')
 // })
 
 async function run() {
-    const newUser = await user.save();
+    // const newUser = await user.save();
+    const newUser = await User.create({
+        name: "Guna",
+        age: 28,
+        hobbies: ['Sports', 'Music'],
+        address: {
+            street: '2nd street',
+        }
+    })
+
     console.log(newUser);
 }
 
